0c1486c3ac28883f275f4a1a6470e36b5ae90562
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-05-10 16:22:38 +0400

    This shadow is pain in the ASSSSS

--- MyWallet/MyWallet/HomeElements/HeaderView.swift
+++ MyWallet/MyWallet/HomeElements/HeaderView.swift
@@ -9,46 +9,50 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        
         HStack {
             Text("My Wallet")
                 .font(.title)
                 .fontWeight(.bold)
-                .padding(.trailing, 80)
                 .foregroundStyle(.white)
-            Button(action: {}) {
-                Image(systemName: "plus")
-            }
+                .padding(.trailing, 50)
             
-            .frame(width: 60, height: 60)
-            .foregroundColor(Color.purple.opacity(0.8))
-            .background(Color.white.opacity(0.2))
-            .clipShape(Circle())
-            .padding(.leading)
-            //Shadow
-            ZStack(alignment: .topTrailing){
+            ZStack {
+                // Actual button with shadow behind
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                        .foregroundColor(Color.purple.opacity(0.8))
+                        .font(.system(size: 24, weight: .bold))
+                }
+                .frame(width: 60, height: 60)
+                .background(Color.secondary)
+                .clipShape(Circle())
+                .shadow(color: Color.white.opacity(0.4), radius: 12, x: -12, y: 6) // Back shadow
+            }
+            .padding(.leading, 10)
 
-                Button(action : {}) {
+            ZStack(alignment: .topTrailing) {
+                Button(action: {}) {
                     Image("person")
                         .resizable()
                         .frame(width: 60, height: 60)
                         .clipShape(Circle())
                         .overlay(
-                            RoundedRectangle(cornerRadius: 30)
+                            Circle()
                                 .stroke(Color.yellow, lineWidth: 1)
-                            )
-                            
+                        )
                 }
+
                 Image(systemName: "crown.fill")
                     .foregroundColor(.yellow)
                     .offset(x: 7, y: -7)
                     .rotationEffect(Angle(degrees: 30))
-
             }
         }
-        
     }
 }
+
+
+
 
 #Preview {
     HeaderView()
