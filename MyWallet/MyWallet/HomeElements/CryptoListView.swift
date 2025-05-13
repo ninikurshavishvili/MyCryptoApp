bc4c528b30367685e6063dfd3b2520c5a1218e85
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-05-13 13:21:27 +0400

    CryptoList View Updates

--- MyWallet/MyWallet/HomeElements/CryptoListView.swift
+++ MyWallet/MyWallet/HomeElements/CryptoListView.swift
@@ -11,38 +11,52 @@ struct CryptoListView: View {
     var cryptoLists: [Crypto]
 
     var body: some View {
-        VStack(spacing: 16) {
-            ForEach(cryptoLists, id: \.self) { crypto in
-                HStack {
-                    Image(crypto.image)
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .foregroundStyle(.secondary)
-
-                    VStack(alignment: .leading, spacing: 4) {
-                        Text(crypto.name)
-                            .font(.headline)
-                        Text(crypto.change)
-                            .foregroundColor(crypto.change.hasPrefix("-") ? .red : .green)
-                    }
-
-                    Spacer()
-
-                    VStack(alignment: .trailing) {
-                        Text(crypto.nameQuantity)
-                        Text(crypto.price)
-                            .foregroundColor(.gray)
-                    }
-
-                    Image(systemName: "chevron.right")
-                        .foregroundColor(.gray)
-                }
+        ZStack {
+            RoundedRectangle(cornerRadius: 24)
+                .fill(Color.gray.opacity(0.1))
                 .padding()
-                .background(.ultraThinMaterial)
-                .cornerRadius(12)
+            VStack(spacing: 16) {
+                ForEach(cryptoLists, id: \.self) { crypto in
+                    HStack {
+                        Image(crypto.image)
+                            .resizable()
+                            .frame(width: 40, height: 40)
+
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(crypto.name)
+                                .foregroundStyle(.white)
+                                .font(.headline)
+                            Text(crypto.change)
+                                .foregroundColor(crypto.change.hasPrefix("-") ? .red : .green)
+                        }
+
+                        Spacer()
+
+                        VStack(alignment: .trailing) {
+                            Text(crypto.nameQuantity)
+                            Text(crypto.price)
+                        }
+                        .foregroundColor(.white)
+
+
+                        VStack {
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                                .padding(.vertical, 15)
+                                .padding(.horizontal, 7)
+                                .background(Color.gray.opacity(0.1))
+                            
+                        }
+                        .cornerRadius(10)
+
+                    }
+                    .padding()
+                    .cornerRadius(12)
+                }
             }
+            .padding()
         }
-        .padding()
+
     }
 }
 
@@ -50,5 +64,6 @@ struct CryptoListView: View {
 
 #Preview {
     CryptoListView(cryptoLists: cryptoList)
+        .background(.black)
 }
 
