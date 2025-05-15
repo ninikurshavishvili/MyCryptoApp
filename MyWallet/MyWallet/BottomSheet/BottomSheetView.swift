03345682fa467cf02cdd505b3d82e9e61936261f
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-05-15 22:05:02 +0400

    SheetView updates

---
+++ MyWallet/MyWallet/BottomSheet/BottomSheetView.swift
@@ -0,0 +1,42 @@
+//
+//  BottomSheetView.swift
+//  MyWallet
+//
+//  Created by Nino Kurshavishvili on 15.05.25.
+//
+
+import SwiftUI
+
+struct BottomSheetView : View {
+    @State private var isShowingSheet = false
+    var body: some View {
+        Button(action: {
+            isShowingSheet.toggle()
+        }) {
+            Text("Show License Agreement")
+        }
+        .sheet(isPresented: $isShowingSheet,
+               onDismiss: didDismiss) {
+            VStack {
+                Text("License Agreement")
+                    .font(.title)
+                    .padding(50)
+                Text("""
+                            Terms and conditions go here.
+                        """)
+                .padding(50)
+                Button("Dismiss",
+                       action: { isShowingSheet.toggle() })
+            }
+        }
+    }
+}
+
+
+    func didDismiss() {
+        // Handle the dismissing action.
+    }
+
+#Preview {
+    BottomSheetView()
+}
