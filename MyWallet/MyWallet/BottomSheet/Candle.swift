e2dc4dd10e36e74073d89c1804802a0c0a0b2deb
ninikurshavishvili <95223541+ninikurshavishvili@users.noreply.github.com>
2025-05-21 16:27:15 +0400

    ChartView customizing

--- MyWallet/MyWallet/BottomSheet/Candle.swift
+++ MyWallet/MyWallet/BottomSheet/Candle.swift
@@ -13,11 +13,18 @@ struct ChartView: View {
     var body: some View {
         VStack {
             Chart {
-                ForEach(candle_BTC) { item in
+                ForEach(candle_btc) { item in
                     RectangleMark(x: .value("Day", item.day),
                                   yStart: .value("lowPrice", item.lowPrice),
                                   yEnd: .value("high price", item.highPrice),
                                   width: 4
+                    )
+                    .opacity(0.4)
+                    
+                    RectangleMark(x: .value("Day", item.day),
+                                  yStart: .value("lowPrice", item.openPrice),
+                                  yEnd: .value("high price", item.closePrice),
+                                  width: 12
                     )
                 }
             }
@@ -42,8 +49,14 @@ struct Candle: Identifiable {
 }
 
 
-var candle_BTC: [Candle] = [Candle(name: "BTC", day: "2025-05-21", lowPrice: 10000, highPrice: 11000, openPrice: 10500, closePrice: 11500),
-                            Candle(name: "ETH", day: "2025-05-25", lowPrice: 10000, highPrice: 11000, openPrice: 10500, closePrice: 11500),
-                            Candle(name: "XRP", day: "2025-01-21", lowPrice: 12000, highPrice: 11000, openPrice: 105020, closePrice: 1150)
-                            
-                            ]
+var candle_btc: [Candle] = [
+    Candle(name: "BTC", day: "0", lowPrice: 3000, highPrice: 7000, openPrice: 3500, closePrice: 6000),
+    Candle(name: "BTC", day: "1", lowPrice: 3000, highPrice: 7000, openPrice: 3500, closePrice: 8000),
+    Candle(name: "BTC", day: "2", lowPrice: 6000, highPrice: 8750, openPrice: 6500, closePrice: 3500),
+    Candle(name: "BTC", day: "3", lowPrice: 2000, highPrice: 4000, openPrice: 2200, closePrice: 3700),
+    Candle(name: "BTC", day: "4", lowPrice: 7000, highPrice: 9000, openPrice: 5900, closePrice: 7000),
+    Candle(name: "BTC", day: "5", lowPrice: 10000, highPrice: 19000, openPrice: 1000, closePrice: 14000)
+]
+
+
+
